5176c1962ae0c205eaddac2ce8e7c88d1438cc0f
Willie <egg358358@gmail.com>
2025-12-12 04:47:57 +0800

    Update packageflow.rb

--- Casks/packageflow.rb
+++ Casks/packageflow.rb
@@ -1,6 +1,6 @@
 cask "packageflow" do
   version "0.1.0"
-  sha256 "43d99969aa22cd414545fb8966c51ffcf10e7ba3604fcbe9af0031ce4acad6b2"
+  sha256 "693eac3bdd0aa77b9053dc5cf0114fd2891ac4e6f8d3166f207aa359fdbddbd4"
 
   url "https://github.com/runkids/PackageFlow/releases/download/v#{version}/PackageFlow_#{version}_aarch64.dmg"
   name "PackageFlow"
